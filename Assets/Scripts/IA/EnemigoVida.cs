a9995042f5e47742f9610c5cbda083b3a6327a4f
BuildTools <unconfigured@null.spigotmc.org>
2024-03-11 22:04:49 -0300

    Agregado vida al enemigo

---
+++ Assets/Scripts/IA/EnemigoVida.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoVida : VidaBase
+{
+    [SerializeField] private EnemigoBarraVida barraVidaPrefab;
+    [SerializeField] private Transform barraVidaPosicion;
+}
